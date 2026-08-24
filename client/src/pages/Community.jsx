502f28fb4f2ca7dc221851d43b5572296cc3890a
Mohd Irfan <mohdirfan10062001@gmail.com>
2026-08-24 16:02:58 +0530

    Fix import statement for Prism in Message component and remove unused import in Community component

--- client/src/pages/Community.jsx
+++ client/src/pages/Community.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { dummyPublishedImages } from "../assets/assets";
 import Loading from "./Loading";
 import { useAppContext } from "../context/AppContext";
 import { toast } from "react-hot-toast";
